5e87b4de50f31151d1a28024ad5b8b789b5e678e
VitalijsVadinskis <vitalijsvadinskis@gmail.com>
2022-01-17 03:31:40 +0200

    VitalijaPraktiskie

---
+++ praktiskais_2/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Hello World from Group group Nr.14 Vitalijs Vadinskis!  </Text>
+      <Text>This is my Vitalijs Vadinskis first React Native application!</Text>
+    </View>
+  )
+}
+export default HelloWorldApp;
